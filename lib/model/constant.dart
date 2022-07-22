796b23bbc79591ea96c73cbff6c3cce25c6633bd
swatsam29 <ssampathkumar@uco.edu>
2022-07-22 12:18:25 -0500

    Final Comment

--- lib/model/constant.dart
+++ lib/model/constant.dart
@@ -2,7 +2,7 @@ class Constant {
   static const devMode = true;
   static const photoFileFolder = 'photo_files';
   static const photoMemoCollection = 'photomemo_collection';
-  static const photodeleted = 'photomemo_deleted ';
+  static const photodeleted = 'photomemo_deleted';
   static const comments = 'photomemo_comments';
   static const users = 'photomemo_users';
   static const notifications = 'photomemo_notifications';
